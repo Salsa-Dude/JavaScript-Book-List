8d1d29826ce4bbcbebe3691616266ca9e5e41b7b
Joseph Arias <arias.joseph.a@gmail.com>
2018-04-20 10:58:38 -0400

    Add Book To List

---
+++ app.js
@@ -0,0 +1,53 @@
+
+// Book Constructor 
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor 
+function UI() {
+
+}
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById('book-list');
+  // Create row element
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+
+  `;
+  list.appendChild(row);
+}
+
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('isbn').value = "";
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e) {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+  
+  // Instantiate Book
+  const book = new Book(title, author,isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+  
+  ui.addBookToList(book);
+  ui.clearFields();
+
+  
+  
+  
+  e.preventDefault();
+});
