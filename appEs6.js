a2fc18be2525ba58731506caf77cdce022513a43
Joseph Arias <arias.joseph.a@gmail.com>
2018-04-22 10:40:16 -0400

    Add Local Storage

--- appEs6.js
+++ appEs6.js
@@ -55,7 +55,49 @@ class UI {
   }
 }
 
-// Event Listeners
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function(book) {
+      const ui = new UI;
+      // Add book to ui
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function(book, index) {
+      if(book.isbn === isbn) {
+        books.splice(index,1);
+      }
+    });
+    
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// Event Listener DOM LOAD event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+// Event Listener add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
@@ -71,6 +113,7 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     ui.showAlert('Please fill in all the fields', 'error')
   } else {
     ui.addBookToList(book);
+    Store.addBook(book);
     ui.showAlert('Book has been Added', 'success');
     ui.clearFields();
   }
@@ -85,6 +128,7 @@ document.getElementById('book-list').addEventListener('click', function(e) {
   // Instantiate UI
   const ui = new UI();
   ui.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
   ui.showAlert('Book has been removed', 'success');
   
   e.preventDefault();
